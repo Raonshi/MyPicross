7f6acb7f9e1f5c951567591ce0fcc81693d2bfea
Raonshi <56419380+Raonshi@users.noreply.github.com>
2021-02-25 20:27:05 +0900

    잠깐 휴식 전 커밋

--- MyPicross/Assets/Scripts/Game.cs
+++ MyPicross/Assets/Scripts/Game.cs
@@ -5,60 +5,37 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public GameObject[,] grid;
+    public GameObject tile;
     public int gridSize;
     public Transform startingPoint;
-
-    public enum Size
-    {
-        Five = 5,
-        Ten = 10,
-        Fifteen = 15,
-        Twenty = 20,
-        Thirty = 30,
-    }
-    public Size size;
 
     // Start is called before the first frame update
     void Start()
     {
+        tile = Resources.Load<GameObject>("Prefabs/Tile");
 
         InitGrid();
+        InitHint();
     }
 
     public void InitGrid()
     {
-        switch(size)
-        {
-            case Size.Five:
-                gridSize = 5;
-                startingPoint.position = new Vector3(4, -3);
-                break;
-            case Size.Ten:
-                gridSize = 10;
-                break;
-            case Size.Fifteen:
-                gridSize = 15;
-                break;
-            case Size.Twenty:
-                gridSize = 20;
-                break;
-            case Size.Thirty:
-                gridSize = 30;
-                break;
-            default:
-                Debug.LogError("Error : Size Error");
-                break;
-        }
-
         grid = new GameObject[gridSize, gridSize];
 
         for(int x = 0; x < gridSize; x++)
         {
             for(int y = 0; y < gridSize; y++)
             {
-
+                GameObject obj = Instantiate(tile, GameObject.Find("Game/Grid").transform);
+                obj.transform.position = new Vector3(startingPoint.position.x + x, startingPoint.position.y + y, 0);
+                obj.name = string.Format("Tile[{0}, {1}]", x, y);
             }
         }
+    }
+
+    public void InitHint()
+    {
+
     }
 
 
---
+++ MyPicross/Assets/Scripts/Title.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnClickStart(int level)
+    {
+        switch(level)
+        {
+            case 0:
+                Debug.Log("Level1 : 5x5 ");
+                break;
+
+            case 1:
+                Debug.Log("Level2 : 10x10 ");
+                break;
+
+            case 2:
+                Debug.Log("Level3 : 15x15 ");
+                break;
+
+            case 3:
+                Debug.Log("Level4 : 20x20 ");
+                break;
+
+            case 4:
+                Debug.Log("Level5 : 30x30 ");
+                break;
+        }
+    }
+}
+
